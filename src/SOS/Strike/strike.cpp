850d5c6fa50268ee009059569d337d9747b176b4
Adeel <3840695+am11@users.noreply.github.com>
2021-10-02 23:14:57 +0300

    sccsid

--- src/SOS/Strike/strike.cpp
+++ src/SOS/Strike/strike.cpp
@@ -10896,7 +10896,7 @@ DECLARE_API(DumpGCData)
 }
 
 #ifdef FEATURE_PAL
-extern char sccsid[];
+extern static char sccsid[];
 #endif
 
 /**********************************************************************\
